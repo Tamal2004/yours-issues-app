a65c56df7769810864335891be281b7534e04b5b
Matt Tamal <matthew.tamal@gmail.com>
2022-04-06 15:28:56 +0600

    Prepare msw with server mocked handlers

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,6 @@
-import '@testing-library/jest-dom/extend-expect'
+import '@testing-library/jest-dom/extend-expect';
+import { server } from './src/utils/testUtils/server';
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
