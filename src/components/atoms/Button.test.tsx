64539bd8bf196475c92afeb6b79861462116e285
Matt Tamal <matthew.tamal@gmail.com>
2022-04-06 07:33:10 +0600

    Implement <Input /> unit tests

--- src/components/atoms/Button.test.tsx
+++ src/components/atoms/Button.test.tsx
@@ -1,48 +1,66 @@
 import React from 'react';
 
-import { render, fireEvent } from 'utils/testUtils';
+import { render, fireEvent, screen } from 'utils/testUtils';
 import Button from './Button';
 
 describe('<Button />', () => {
-    it('should render a button', () => {
-        const mockClick = jest.fn();
-        const { getByText } = render(<Button onClick={mockClick}>Show</Button>);
+    const buttonName = 'Issue'; // Accessibility name
 
-        const TestButton = getByText('Show');
-        expect(TestButton).toBeInTheDocument();
-        expect(TestButton).toMatchSnapshot();
+    it('should render a button', () => {
+        const clickHandler = jest.fn();
+        render(
+            <Button aria-label={buttonName} onClick={clickHandler}>
+                Issue
+            </Button>
+        );
+
+        const issueButton = screen.getByRole('button', { name: buttonName });
+
+        expect(issueButton).toBeInTheDocument();
+        expect(issueButton).toMatchInlineSnapshot(`
+            <button
+              aria-label="Issue"
+              class="sc-gsnTZi goDtiU"
+              role="button"
+            >
+              Issue
+            </button>
+        `);
     });
 
     it('should perform the passed on click function', () => {
-        const mockClick = jest.fn();
-        const { getByText } = render(<Button onClick={mockClick}>Show</Button>);
-        fireEvent.click(getByText('Show'));
-        expect(mockClick.mock.calls.length).toBe(1);
+        const clickHandler = jest.fn();
+        render(
+            <Button aria-label={buttonName} onClick={clickHandler}>
+                Issue
+            </Button>
+        );
+
+        fireEvent.click(screen.getByRole('button', { name: buttonName }));
+        expect(clickHandler).toHaveBeenCalledTimes(1);
     });
 
     it('should not perform passed on click function when disabled', () => {
-        const mockClick = jest.fn();
-
-        const { getByText } = render(
-            <Button onClick={mockClick} disabled>
-                Show
+        const clickHandler = jest.fn();
+        render(
+            <Button aria-label={buttonName} onClick={clickHandler} disabled>
+                Issue
             </Button>
         );
 
-        fireEvent.click(getByText('Show'));
-        expect(mockClick.mock.calls.length).toBe(0);
+        fireEvent.click(screen.getByRole('button', { name: buttonName }));
+        expect(clickHandler).toHaveBeenCalledTimes(0);
     });
 
     it('should not perform passed on click function when loading', () => {
-        const mockClick = jest.fn();
-
-        const { getByText } = render(
-            <Button onClick={mockClick} loading>
-                Show
+        const clickHandler = jest.fn();
+        render(
+            <Button aria-label={buttonName} onClick={clickHandler} loading>
+                Issue
             </Button>
         );
 
-        fireEvent.click(getByText('Show'));
-        expect(mockClick.mock.calls.length).toBe(0);
+        fireEvent.click(screen.getByRole('button', { name: buttonName }));
+        expect(clickHandler).toHaveBeenCalledTimes(0);
     });
 });
