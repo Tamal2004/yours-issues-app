a16c8ad213728447759eb6677f0686b6e902262e
Matt Tamal <matthew.tamal@gmail.com>
2022-04-06 17:21:11 +0600

    Finalize README.md

--- src/features/issues/IssuesPage/index.tsx
+++ src/features/issues/IssuesPage/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactPaginate from 'react-paginate';
 
 import { styled } from 'theme';
 import { useAppSelector } from 'utils/hooks/store';
@@ -7,6 +6,7 @@ import { useAppSelector } from 'utils/hooks/store';
 import Layout from 'templates/Layout';
 
 import Issues from './Issues';
+import Pagination from './Pagination';
 import { usePaginateProps, useRerouteIfFreshShallowRender } from './hooks';
 
 const Background = styled(Layout)`
@@ -40,112 +40,6 @@ const IssuesCount = styled.span`
     color: ${({ theme }) => theme.colors.grey.lightest};
 
     ${({ theme }) => theme.typography.button};
-`;
-
-const Pagination = styled(ReactPaginate)`
-    display: grid;
-    grid-auto-flow: column;
-    grid-auto-columns: max-content;
-    grid-column-gap: 0.5rem;
-    justify-content: center;
-
-    padding: 5rem 0 0;
-    list-style: none;
-
-    & .active {
-        background-color: ${({ theme }) => theme.colors.primary.main};
-    }
-
-    & > li {
-        border: 1px solid transparent;
-        border-radius: 0.75rem;
-
-        &:not(.page-break, .disabled) {
-            cursor: pointer;
-        }
-
-        &:hover:not(.page-break, .disabled) {
-            border-color: ${({ theme }) => theme.colors.border.light};
-        }
-    }
-
-    .page-break {
-        pointer-events: none;
-        cursor: initial;
-    }
-
-    & .page-link {
-        padding: 0.625rem 1.25rem;
-        color: ${({ theme }) => theme.colors.grey.lightest};
-    }
-
-    & .page-item {
-        display: flex;
-        ${({ theme }) => theme.typography.paginate};
-    }
-
-    & .active {
-        background-color: ${({ theme }) => theme.colors.primary.main};
-
-        color: ${({ theme }) => theme.colors.grey.main};
-    }
-
-    & .page-previous,
-    .page-next {
-        display: flex;
-        align-items: center;
-
-        &::before,
-        &::after {
-            margin: 0 0.875rem;
-        }
-
-        &:not(.disabled) {
-            color: ${({ theme }) => theme.colors.primary.main};
-            & .page-link {
-                color: inherit;
-            }
-
-            &::before,
-            &::after {
-                border-color: ${({ theme }) => theme.colors.primary.main};
-            }
-        }
-    }
-
-    & .page-previous {
-        & .page-link {
-            padding-left: 0;
-        }
-
-        &::before {
-            content: '';
-            display: block;
-            width: 0.875rem;
-            height: 0.875rem;
-            border: 2px solid ${({ theme }) => theme.colors.grey.lightest};
-            border-right: initial;
-            border-top: initial;
-            transform: rotate(45deg);
-        }
-    }
-
-    & .page-next {
-        & .page-link {
-            padding-right: 0;
-        }
-
-        &::after {
-            content: '';
-            display: block;
-            width: 0.875rem;
-            height: 0.875rem;
-            border: 2px solid ${({ theme }) => theme.colors.grey.lightest};
-            border-left: initial;
-            border-bottom: initial;
-            transform: rotate(45deg);
-        }
-    }
 `;
 
 const IssuesPage: React.FC = () => {
