3b758122dec3a12cc3fa12e8adf9a029f3897808
Matt Tamal <matthew.tamal@gmail.com>
2022-04-06 09:53:36 +0600

    Implement Issues Tab Header

--- src/pages/[owner]/[repository].tsx
+++ src/pages/[owner]/[repository].tsx
@@ -1,3 +1,21 @@
 import IssuesPage from 'features/issues/IssuesPage';
 
+import { wrapper } from 'utils/store';
+import { fetchRepository } from 'features/issues/issuesSlice/actions';
+import { RepositoryAttributes } from 'types/issues';
+
+export const getServerSideProps = wrapper.getServerSideProps((store) =>
+    // @ts-ignore
+    async (context) => {
+        const { owner, repository } =
+            context.params as unknown as RepositoryAttributes;
+        const { shallow = false } = context.query;
+
+        if (!shallow) {
+            await store.dispatch(fetchRepository({ owner, repository }));
+        }
+        return {};
+    }
+);
+
 export default IssuesPage;
